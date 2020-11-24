3128204ab64719c214f82d6b462db2ade2f08105
Gabriela Johnson <johnsongabriela12@gmail.com>
2020-11-24 13:21:03 -0800

    adds new book ation to controller and adds the book params in the private method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,19 @@ class BooksController < ApplicationController
 
     def index
     end 
+
+    def new
+        @book= Book.new 
+        #use instance varialbe in views
+    end 
+
+    def create
+        @book=Book.new(book_params)
+    end 
+
+    private
+
+        def book_params
+            params.require(:book).permit(:title, :description, :author)
+        end 
 end

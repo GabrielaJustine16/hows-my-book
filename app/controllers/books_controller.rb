4f158be63c452457601846acc9e0db9b5fc78312
Gabriela Johnson <johnsongabriela12@gmail.com>
2020-11-24 16:28:47 -0800

    adds link to to the index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
+    before_action :find_book, only: [:show,:edit,:update,:destroy]
 
     def index
         @books=Book.all.order("created_at DESC")
     end 
     #will be listed in descending order
+
+    def show
+        
+
+    end 
 
     def new
         @book= Book.new 
@@ -26,4 +32,8 @@ class BooksController < ApplicationController
         def book_params
             params.require(:book).permit(:title, :description, :author)
         end 
+
+        def find_book
+            @book=Book.find(params[:id])
+        end
 end

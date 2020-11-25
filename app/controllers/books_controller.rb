9adff1cffcc8ab34c5fd92344757eb3b87ac360f
Gabriela Johnson <johnsongabriela12@gmail.com>
2020-11-24 16:15:38 -0800

    adds add bok to the index html page and adds show action to book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,9 @@
 class BooksController < ApplicationController
 
     def index
+        @books=Book.all.order("created_at DESC")
     end 
+    #will be listed in descending order
 
     def new
         @book= Book.new 
@@ -10,10 +12,17 @@ class BooksController < ApplicationController
 
     def create
         @book=Book.new(book_params)
+
+        if @book.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
     end 
+    #instead of making a create.html page, we will have this redirect to the root path when the create button is hit. in rake routes, root is the path to the index page which is the home page
 
     private
-
+ 
         def book_params
             params.require(:book).permit(:title, :description, :author)
         end 
